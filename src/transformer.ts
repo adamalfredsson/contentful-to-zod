41cd22f2f068285248fee1877a496f3933e19fbd
Adam Alfredsson <alfredsson.adam@gmail.com>
2024-12-08 12:19:05 +0100

    Fix content type schema

--- src/transformer.ts
+++ src/transformer.ts
@@ -70,19 +70,7 @@ function createReferencedFieldSchema<TKey extends string>(
   contentType: TKey,
   schema: z.ZodObject<z.ZodRawShape>
 ): z.ZodType {
-  return augmentSchemaWithReference(
-    z.object({
-      sys: z.object({
-        contentType: z.object({
-          sys: z.object({
-            id: z.literal(contentType),
-          }),
-        }),
-      }),
-      fields: schema,
-    }),
-    contentType
-  );
+  return augmentSchemaWithReference(schema, contentType);
 }
 
 /**
